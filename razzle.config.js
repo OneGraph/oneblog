25868009aac0b384464c93d387e18c1a39c80fdf
Daniel Woelfel <dwwoelfel@gmail.com>
2019-08-20 20:04:53 -0700

    silence performance warning

--- razzle.config.js
+++ razzle.config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+  plugins: [WebpackPerformanceHintsRazzlePlugin({})],
+};
+
+function WebpackPerformanceHintsRazzlePlugin(pluginOptions) {
+  return function WebpackPerformanceHintsRazzlePluginFunc(config) {
+    return {
+      ...config,
+      performance: {
+        ...config.performance,
+        assetFilter: function(assetFilename) {
+          return false;
+        },
+      },
+    };
+  };
+}
