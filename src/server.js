b4aced8c1331cf8162b851b9ea865250049609bd
Daniel Woelfel <dwwoelfel@gmail.com>
2019-12-02 17:43:06 -0800

    add google analytics

--- src/server.js
+++ src/server.js
@@ -59,6 +59,19 @@ function buildHtml({markup, styleTags, bootstrapData, basePath}) {
         ? `<script src="${assets.client.js}" defer></script>`
         : `<script src="${assets.client.js}" defer crossorigin></script>`
     }
+    ${
+      process.env.NODE_ENV === 'production'
+        ? `
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122815795-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-122815795-1');
+</script>`
+        : ''
+    }
   </head>
   <body>
     <div id="root">${markup ? markup : ''}</div>
