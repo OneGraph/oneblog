023f3fa77b2e69b9fc8421ace9b7f5ddea4e6263
Daniel Woelfel <dwwoelfel@gmail.com>
2020-10-26 14:38:39 -0700

    add endpoint to send config information

---
+++ src/pages/api/config.js
@@ -0,0 +1,16 @@
+// @flow
+
+import Config from '../../config';
+
+export default (req, res) => {
+  res.setHeader('access-control-allow-origin', '*');
+  res.json({
+    version: '1.0.0',
+    oneGraphAppId: Config.appId,
+    repoName: Config.repoName,
+    repoOwner: Config.repoOwner,
+    codeTheme: Config.codeTheme,
+    title: Config.title,
+    description: Config.description,
+  });
+};
